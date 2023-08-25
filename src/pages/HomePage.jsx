958e5ca5e3a467673f737b17d836a0d913d1d613
Edna Vidal <ednavidalros@gmail.com>
2023-08-25 15:17:07 +0200

    "iteration 4"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,49 @@
-function HomePage() {}
+import CountryFlag from 'react-country-flag';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+
+function HomePage() {
+
+    const [countries, setCountries] = useState([])
+    const apiUrl = "https://ih-countries-api.herokuapp.com/countries"
+
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setCountries(data)
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+    function toLowerCase(str) {
+        return str.toLowerCase()
+    }
+    
+    return (
+        <>
+            <div className="container" style={{ maxHeight: "90vh", overflow: "scroll" }}>
+                <h1 style={{ fontSize: "24px" }}>WikiCountries: Your Guide to the World</h1>
+                <div className="list-group">
+                 {   countries.map((country) => (
+                        <div key={country.alpha3Code}>
+                            <Link className="list-group-item list-group-item-action" to={`/${country.alpha3Code}`}> <img style={{height: "15px"}} src={`https://flagpedia.net/data/flags/icon/72x54/${toLowerCase(country.alpha2Code)}.png`} alt="" />
+                                {country.name.common}</Link>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+
+}
 
 export default HomePage;
